c65a06f5a999fd7b9a1ae8d92f9e23656d5338e1
ismailrabie <ismailrabie111@gmail.com>
2024-12-02 13:42:02 +0200

    show showModalBottomSheet and put  floatingActionButton

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/views/notes_view_body.dart';
 
+import '../widget/add_note_bootom_sheet.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(body:  NotesViewBody());
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showModalBottomSheet(context: context, builder: (context) {
+          return AddNoteBottomSheet();
+        },);
+
+      },child: Icon(Icons.add),),
+        body:  NotesViewBody());
   }
 }
+
+

7ae72a4decac44644af72e33cd5f8970567de61f
ismailrabie <ismailrabie111@gmail.com>
2024-12-03 14:09:50 +0200

    create NoteBottomSheet

--- lib/widget/add_note_bootom_sheet.dart
+++ lib/widget/add_note_bootom_sheet.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/constans.dart';
+import 'package:noteapp/widget/custom_text_filed.dart';
+
+import 'custom_bottom.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextFiled(
+              hint: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFiled(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(height: 30,),
+            
+            CustomBottom(),
+            SizedBox(height: 20,)
+          ],
+        ),
+      ),
+    );
   }
 }

446fec49620b8cea052654c31a43183d7ad0f81e
ismailrabie <ismailrabie111@gmail.com>
2024-12-03 14:10:59 +0200

    edit NoteItem

--- lib/views/custom_notes_item.dart
+++ lib/views/custom_notes_item.dart
@@ -1,44 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: Color(0xFFffcc80), borderRadius: BorderRadius.circular(17)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              "Flutter Tips",
-              style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+            color: const Color(0xFFffcc80), borderRadius: BorderRadius.circular(17)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                "Flutter Tips",
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text("Build you carere here bro and yeah",
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(0.5), fontSize: 18)),
+              ),
+              trailing: Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 28,
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text("Build you carere here bro and yeah",
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(0.5), fontSize: 18)),
-            ),
-            trailing: Icon(
-              FontAwesomeIcons.trash,
-              color: Colors.black,
-              size: 28,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "AUG,2022",
-              style:
-              TextStyle(fontSize: 16, color: Colors.black.withOpacity(0.4)),
-            ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                "AUG,2022",
+                style: TextStyle(
+                    fontSize: 16, color: Colors.black.withOpacity(0.4)),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

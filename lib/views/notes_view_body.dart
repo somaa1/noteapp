3ce7225267a26e5e7f690d39fdb1de7fccdcfe5b
ismailrabie <ismailrabie111@gmail.com>
2024-12-03 14:13:55 +0200

    Edit NotesViewBody

--- lib/views/notes_view_body.dart
+++ lib/views/notes_view_body.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:noteapp/widget/custom_app_bar.dart';
+
+import '../widget/notes_list_view.dart';
+import 'custom_notes_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
-
-  @override
+@override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: const Column(
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
         children: [
-          SizedBox(height: 50,),
-      CustomAppBar()
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(title: 'Notes', iconData: FontAwesomeIcons.trash,),
+          Expanded(child: NotesListView())
         ],
       ),
     );
   }
 }
+
+
+
+

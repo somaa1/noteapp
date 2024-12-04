0870aed8e810044315dcad8ae81afa1b7789cd92
ismailrabie <ismailrabie111@gmail.com>
2024-12-04 12:30:58 +0200

    add GestureDetector t button

--- lib/widget/custom_bottom.dart
+++ lib/widget/custom_bottom.dart
@@ -4,22 +4,25 @@ import 'package:flutter/material.dart';
 import '../constans.dart';
 
 class CustomBottom extends StatelessWidget {
-  const CustomBottom({super.key});
-
+  const CustomBottom({super.key,  required this.ontap});
+final void Function()? ontap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 56,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: KprimeColor,
-        ),
-        child: Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap:ontap ,
+      child: Container(
+          height: 56,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: kPrimeColor,
           ),
-        ));
+          child: Center(
+            child: Text(
+              'Add',
+              style: TextStyle(
+                  color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          )),
+    );
   }
 }

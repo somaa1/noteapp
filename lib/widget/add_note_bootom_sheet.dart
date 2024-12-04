f867a2b52849d26c7af5ecd7683c4cb8cba2ca53
ismailrabie <ismailrabie111@gmail.com>
2024-12-04 12:28:27 +0200

    validate user input in add note

--- lib/widget/add_note_bootom_sheet.dart
+++ lib/widget/add_note_bootom_sheet.dart
@@ -9,30 +9,64 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextFiled(
-              hint: 'Title',
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextFiled(
-              hint: 'Content',
-              maxLines: 5,
-            ),
-            SizedBox(height: 30,),
-            
-            CustomBottom(),
-            SizedBox(height: 20,)
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+final GlobalKey<FormState>formkey = GlobalKey();
+AutovalidateMode autovalidateMode=AutovalidateMode.disabled;
+String? title,suptitle;
+class _AddNoteFormState extends State<AddNoteForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      autovalidateMode:autovalidateMode ,
+      key: formkey,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextFiled(
+            onsaved: (value) {
+              title = value;
+            },
+            hint: 'Title',
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextFiled(
+            onsaved: (value) {
+              suptitle = value;
+            },
+            hint: 'Content',
+            maxLines: 5,
+          ),
+          const SizedBox(height: 30,),
+
+          CustomBottom(ontap: () {
+            if(formkey.currentState!.validate()){
+              formkey.currentState!.save();
+            }else{
+             autovalidateMode= AutovalidateMode.always;
+            }
+          },),
+          const SizedBox(height: 20,)
+        ],
       ),
     );
   }

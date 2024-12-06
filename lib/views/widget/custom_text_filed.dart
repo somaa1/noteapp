34dca99c224f038eff966ef4513e2cda7f05921a
ismailrabie <ismailrabie111@gmail.com>
2024-12-06 17:13:45 +0200

    Edit custom_text_filed

--- lib/views/widget/custom_text_filed.dart
+++ lib/views/widget/custom_text_filed.dart
@@ -3,38 +3,47 @@ import 'package:flutter/material.dart';
 import '../../constans.dart';
 
 class CustomTextFiled extends StatelessWidget {
-  const CustomTextFiled({super.key, required this.hint,  this.maxLines=1, this.onsaved});
-final String hint;
-final int maxLines;
-final void Function(String?)? onsaved;
+  const CustomTextFiled(
+      {super.key,
+      required this.hint,
+      this.maxLines = 1,
+      this.onsaved,
+      this.onChanged,
+    });
+
+  final String hint;
+
+  final int maxLines;
+  final void Function(String?)? onsaved;
+  final void Function(String)? onChanged;
+
   @override
   Widget build(BuildContext context) {
-    return  TextFormField(
-      onSaved:onsaved ,
-      validator: (value) {
-        if (value?.isEmpty ?? true){
-          return "Please fill this field";
-        }else{
-          return null;  // No validation errors
-        }
-      },
-      maxLines:maxLines ,
-      decoration:InputDecoration(
-        border: buildBorder(),
-        enabledBorder: buildBorder(),
-        focusedBorder: buildBorder(color:kPrimeColor),
-        hintText: hint,
-     //   hintStyle: TextStyle(color: KprimeColor),
+    return TextFormField(
+        onChanged: onChanged,
+        onSaved: onsaved,
+        validator: (value) {
+          if (value?.isEmpty ?? true) {
+            return "Please fill this field";
+          } else {
+            return null; // No validation errors
+          }
+        },
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          focusedBorder: buildBorder(color: kPrimeColor),
+          hintText: hint,
 
-      )
 
-    );
+          //   hintStyle: TextStyle(color: KprimeColor),
+        ));
   }
 
   OutlineInputBorder buildBorder({color}) {
     return OutlineInputBorder(
         borderRadius: BorderRadius.circular(6),
-borderSide: BorderSide(color: color ?? Colors.white)
-      );
+        borderSide: BorderSide(color: color ?? Colors.white));
   }
 }

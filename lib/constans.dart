44cf27696949b8375adb6150bc8f94d8d38fddbf
ismailrabie <ismailrabie111@gmail.com>
2024-12-06 16:54:01 +0200

    Add color List in constants

--- lib/constans.dart
+++ lib/constans.dart
@@ -2,3 +2,10 @@
 
 const   kPrimeColor    = Color(0Xff62FcD7);
 const   kNoteBox   = 'notes_box';
+ List<Color> kColors = const [
+   Color(0xffAC3931),
+   Color(0xffE5D352),
+   Color(0xffD9E76C),
+   Color(0xff537D8D),
+   Color(0xff482C3D),
+ ];

a8e8cbb589b26bdf906d337ccc67406baf2925c8
ismailrabie <ismailrabie111@gmail.com>
2024-12-06 17:17:38 +0200

    Edit edit_note_view_body

--- lib/views/edit_note_view_body.dart
+++ lib/views/edit_note_view_body.dart
@@ -1,35 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/views/widget/custom_app_bar.dart';
 import 'package:noteapp/views/widget/custom_text_filed.dart';
+import 'package:noteapp/views/widget/edit_note_colors_list_view.dart';
 
-class EditNoteViewBody extends StatelessWidget {
-  const EditNoteViewBody({super.key});
+import '../cubits/notes_cubit/notes_cubit.dart';
+
+class EditNoteViewBody extends StatefulWidget {
+  const EditNoteViewBody({super.key, required this.note});
+
+  final NoteModel note;
+
+  @override
+  State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
+}
+
+class _EditNoteViewBodyState extends State<EditNoteViewBody> {
+  String? title, content;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 50,
           ),
           CustomAppBar(
+            onPressed: () {
+              widget.note.title = title ?? widget.note.title;
+              widget.note.subtitle = content ?? widget.note.subtitle;
+              widget.note.save();
+              BlocProvider.of<NotesCubit>(context).fetchAllData();
+              Navigator.pop(context);
+            },
             title: 'Edit Note',
             iconData: Icons.check,
           ),
-          SizedBox(
+          const SizedBox(
             height: 50,
           ),
-          CustomTextFiled(hint: 'Title'),
-          SizedBox(
+          CustomTextFiled(
+            hint: widget.note.title,
+            onChanged: (value) {
+              title = value;
+            },
+          ),
+          const SizedBox(
             height: 30,
           ),
-
-
-          CustomTextFiled(hint: 'Content'),
-
+          CustomTextFiled(
+            hint: widget.note.subtitle,
+            onChanged: (value) {
+              content = value;
+            },
+          ),
+          EditNoteColorsList(
+            note: widget.note,
+          )
         ],
       ),
     );

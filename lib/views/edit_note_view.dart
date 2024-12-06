2bcac19fd28eb484adc18dd62221c923fa450f39
ismailrabie <ismailrabie111@gmail.com>
2024-12-06 17:16:49 +0200

    create edit_note_view

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/views/edit_note_view_body.dart';
 
 class EditNoteView extends StatelessWidget {
-  const EditNoteView({super.key});
-
+  const EditNoteView({super.key, required this.note});
+final NoteModel note;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const EditNoteViewBody());
+    return Scaffold(body:  EditNoteViewBody(note: note,));
   }
 }

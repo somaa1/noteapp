d8c5318ce1b52c138ec9f1398e6721af5ce03405
ismailrabie <ismailrabie111@gmail.com>
2024-12-06 16:48:01 +0200

    some Edit in Add note Form

--- lib/views/widget/add_note_form.dart
+++ lib/views/widget/add_note_form.dart
@@ -1,5 +1,9 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:noteapp/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:noteapp/models/note_model.dart';
+import 'package:intl/intl.dart';
+import 'package:noteapp/views/widget/color_list_view.dart';
 
 import 'custom_bottom.dart';
 import 'custom_text_filed.dart';
@@ -12,14 +16,16 @@ class AddNoteForm extends StatefulWidget {
   @override
   State<AddNoteForm> createState() => _AddNoteFormState();
 }
-final GlobalKey<FormState>formkey = GlobalKey();
-AutovalidateMode autovalidateMode=AutovalidateMode.disabled;
-String? title,suptitle;
+
+final GlobalKey<FormState> formkey = GlobalKey();
+AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+String? title, subtitle;
+
 class _AddNoteFormState extends State<AddNoteForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      autovalidateMode:autovalidateMode ,
+      autovalidateMode: autovalidateMode,
       key: formkey,
       child: Column(
         children: [
@@ -37,23 +43,46 @@ class _AddNoteFormState extends State<AddNoteForm> {
           ),
           CustomTextFiled(
             onsaved: (value) {
-              suptitle = value;
+              subtitle = value;
             },
             hint: 'Content',
             maxLines: 5,
           ),
-          const SizedBox(height: 30,),
+          const SizedBox(
+            height: 30,
+          ),
+          ColorsListView(),
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CustomBottom(
+                isloading: state is AddNoteLoading ? true : false,
+                ontap: () {
+                  if (formkey.currentState!.validate()) {
 
-          CustomBottom(ontap: () {
-            if(formkey.currentState!.validate()){
-              formkey.currentState!.save();
-            }else{
-              autovalidateMode= AutovalidateMode.always;
-            }
-          },),
-          const SizedBox(height: 20,)
+                    var currentDate = DateTime.now();
+
+                    var formattedCurrentDate =
+                    DateFormat('dd-MM-yyyy', 'en_US').format(currentDate);
+                    formkey.currentState!.save();
+                    var modelNote = NoteModel(
+                        title: title!,
+                        subtitle: subtitle!,
+                        data: formattedCurrentDate,
+                        color: Colors.redAccent.value);
+                    BlocProvider.of<AddNoteCubit>(context).addNote(modelNote);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                  }
+                },
+              );
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          )
         ],
       ),
     );
   }
 }
+

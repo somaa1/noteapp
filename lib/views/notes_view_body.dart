271d7bf8d0b503a562f6b7c4ece42df8464a8ba4
ismailrabie <ismailrabie111@gmail.com>
2024-12-06 17:18:33 +0200

    edit notes_view_body.dart

--- lib/views/notes_view_body.dart
+++ lib/views/notes_view_body.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:noteapp/widget/custom_app_bar.dart';
-
-import '../widget/notes_list_view.dart';
+import 'package:noteapp/views/widget/custom_app_bar.dart';
+import 'package:noteapp/views/widget/notes_list_view.dart';
+import '../cubits/notes_cubit/notes_cubit.dart';
 import 'custom_notes_item.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   const NotesViewBody({super.key});
+
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllData();
+    super.initState();
+  }
+
 @override
   Widget build(BuildContext context) {
     return const Padding(

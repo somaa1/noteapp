08dad0b58e6706d60b659b1814f77e7a477ee3aa
ismailrabie <ismailrabie111@gmail.com>
2024-12-07 11:01:47 +0200

    edit note_view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/views/notes_view_body.dart';
+import 'package:noteapp/views/widget/add_note_bootom_sheet.dart';
 
-import '../widget/add_note_bootom_sheet.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -9,20 +9,23 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
+
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             showModalBottomSheet(
+              isScrollControlled: true,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(16),
               ),
               context: context,
               builder: (context) {
-                return AddNoteBottomSheet();
+                return const AddNoteBottomSheet();
               },
             );
           },
           child: Icon(Icons.add),
         ),
-        body: NotesViewBody());
+        body: const NotesViewBody());
   }
 }

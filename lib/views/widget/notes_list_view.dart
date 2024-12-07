dc13a2663d93e3af6ef55279817ed29932d6ddee
ismailrabie <ismailrabie111@gmail.com>
2024-12-07 10:57:47 +0200

    edit note list view

--- lib/views/widget/notes_list_view.dart
+++ lib/views/widget/notes_list_view.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:noteapp/cubits/notes_cubit/notes_cubit.dart';
+import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/views/custom_notes_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
-  final  data=const[];
+
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder( itemBuilder: (context,index) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 5),
-        child: NoteItem(),
-      );
-    });
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes=BlocProvider.of<NotesCubit>(context).notes ?? [];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+            itemCount: notes.length,
+              itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: NoteItem(note: notes[index],),
+            );
+          }),
+        );
+      },
+    );
   }
 }
